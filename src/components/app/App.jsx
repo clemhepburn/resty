f345f84567eaf6518dd58211c51eb65c7e42b4b9
Clem Hepburn <clemhepburn@gmail.com>
2021-08-20 11:48:11 -0700

    wire in container, working so far

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
+import ReposanteContainer from '../../containers/ReposanteContainer';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return <ReposanteContainer />;
 }
